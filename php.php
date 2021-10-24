a7a32f4e456e4669f5f61d7457f2c140e4ec24bb
Sasha <kremlink0@gmail.com>
2021-10-25 02:54:13 +0300

    full article pop

--- php.php
+++ php.php
@@ -1,6 +1,6 @@
 {
  "user":{"sid":12345,"code":"12345:2"},
- "achievements":[{"h":"Спасены","text":"Текст1","img":"images/achievements/ach1.png"},{"h":"Упаковано","text":"Текст2","img":"images/achievements/ach1.png"},
+ "achievements":[{"h":"Спасены","text":"Текст1 Текст1 Текст1 Текст1 Текст1 Текст1 Текст1","img":"images/achievements/ach1.png"},{"h":"Упаковано","text":"Текст2","img":"images/achievements/ach1.png"},
  {"disabled":true,"h":"Общий сбор","text":"Текст3","img":"images/achievements/ach1.png"},{"h":"План","text":"Текст4","img":"images/achievements/ach1.png"}],
  "achievement":{"h":"План","text":"Текст","img":"images/achievements/ach1.png"},
  "articles":[{"bg":"images/startscreen-btn.png","icon":"images/info-pop/pers1.jpg","h":"Сохранение","text":"Поход в лес может закончиться печально, если не быть готовым к неожиданностям.","pop":[{"text":"Text1"},{"img":"images/startscreen-btn-h.png"}]}],
